4b6c806c51fd7acebe814b2f4672eeb1d5786f9e
Dhruv Garg <63773208+vrudhgarg@users.noreply.github.com>
2025-03-11 21:50:52 -0700

    This commit will create the basic repo

---
+++ src/functions/support.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+def month_year_generator(start_month, start_year, end_month, end_year):
+    """Generates ('Mon', Year) tuples from start to end month-year."""
+    current = datetime(start_year, start_month, 1)
+    end = datetime(end_year, end_month, 1)
+
+    while current <= end:
+        yield current.strftime("%b"), current.year  
+        # Move to next month
+        if current.month == 12:
+            current = datetime(current.year + 1, 1, 1)
+        else:
+            current = datetime(current.year, current.month + 1, 1)
